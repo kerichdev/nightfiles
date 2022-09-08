01eb43033f9401d90fc9b5226e4bf95e329b9c9b
Keri <kerichuudev@gmail.com>
2022-09-08 15:40:21 +0300

    New icon method + fixes

--- .config/polybar/bt.sh
+++ .config/polybar/bt.sh
@@ -3,67 +3,20 @@ source ~/.config/polybar/colors.sh
 DEVICE=`bluetoothctl info | awk -F 'Alias: ' '{print $2}' | tr -d " \t\n"`
 ICON=`bluetoothctl info | awk -F 'Icon: ' '{print $2}' | tr -d " \t\n"`
 CHARGE=`upower -d | awk -F 'percentage: ' '{print $2}' | tr -d " \t\n" | cut -f1 -d"%"`
-round() { awk -v n=$1 -v d=$2 'BEGIN{print int((n+d/2)/d) * d}'; }
 
-CHARGERND=`round $CHARGE 10`
+BATTERY_ICONS=("󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 
-case $CHARGERND in
-  0)
-	CICON=%{F$COLOR}󰂎%{F-}
-	;;
-
-  10)
-    CICON=%{F$COLOR}󰁺%{F-}
-    ;;
-
-  20)
-    CICON=%{F$COLOR}󰁻%{F-}
-    ;;
-
-  30)
-    CICON=%{F$COLOR}󰁼%{F-}
-    ;;
-
-  40)
-    CICON=%{F$COLOR}󰁽%{F-}
-    ;;
-
-  50)
-    CICON=%{F$COLOR}󰁾%{F-}
-    ;;
-
-  60)
-    CICON=%{F$COLOR}󰁿%{F-}
-    ;;
-
-  70)
-    CICON=%{F$COLOR}󰂀%{F-}
-    ;;
-
-  80)
-    CICON=%{F$COLOR}󰂁%{F-}
-    ;;
-
-  90)
-    CICON=%{F$COLOR}󰂂%{F-}
-    ;;
-
-  100)
-    CICON=%{F$COLOR}󰁹%{F-}    
-    ;;
-
-esac
+C_INDEX=$((CHARGE * 11 / 100))
+C_ICON=%{F$COLOR}${BATTERY_ICONS[$C_INDEX]}%{F-}
 
 if [ "$DEVICE" == "" ]
 	then echo "%{F$COLOR}󰂲%{F-}"
 	else 
 		if [ "$ICON" == "audio-headphones" ]
-			then echo "%{F$COLOR}󰂰%{F-} $DEVICE"
+			then echo "%{F$COLOR}󰂰%{F-} $DEVICE ($C_ICON$CHARGE%)"
 		else if [ "$ICON" == "input-keyboard" ]
-			then echo "%{F$COLOR}󰂱%{F-} $DEVICE ($CICON$CHARGE%)"
+			then echo "%{F$COLOR}󰂱%{F-} $DEVICE ($C_ICON$CHARGE%)"
 		else echo "%{F$COLOR}󰂯%{F-} $DEVICE"
 		fi
 	fi
 fi
-
-#%{F$COLOR}󰂲%{F-}
