5a31fe6994c3a93dcdb743bf1b1cf515680596d2
kerichdev <kerichuudev@gmail.com>
2022-09-14 17:02:46 +0300

    entirely new icon implementation

--- .config/polybar/bt.sh
+++ .config/polybar/bt.sh
@@ -5,39 +5,28 @@ DEVICE=`bluetoothctl info | awk -F 'Alias: ' '{print $2}' | tr -d " \t\n"`
 
 OUTPUTARRAY=()
 FINAL=()
+
 LOOPWATCHDOG=0
 
-for COUNT in $COUNT_CONNECTED; do
-	OUTPUTARRAY+=($(bluetoothctl devices Connected | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep "Icon\|Battery Percentage\|Connected" | sed -n -e 's/^.*Icon: //p' | sed -n ${COUNT}p) $(bluetoothctl devices Connected | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep "Icon\|Battery Percentage\|Connected" | sed -n -e 's/^.*Battery Percentage: //p' | sed -n ${COUNT}p | sed 's/(\(.*\))/\1/' | sed 's/[^ ]* //'))
-done
+REXP='^[0-9]+$'
 
-for ELEMENT in ${OUTPUTARRAY[@]}; do
-	case $ELEMENT in
-		"input-keyboard") FINAL+=("%{F$COLOR}󰌌%{F-}" ${OUTPUTARRAY[LOOPWATCHDOG+1]}%)
-		((LOOPWATCHDOG=LOOPWATCHDOG+2))
-		FINAL+=("• ")
-		;;
-		"audio-headphones") FINAL+=("%{F$COLOR}󰋋%{F-}" ${OUTPUTARRAY[LOOPWATCHDOG+1]}%)
-		((LOOPWATCHDOG=LOOPWATCHDOG+2))
-		FINAL+=("• ")
-		;;
-		"audio-card") FINAL+=("%{F$COLOR}󰓃%{F-}" ${OUTPUTARRAY[LOOPWATCHDOG+1]}%)
-		((LOOPWATCHDOG=LOOPWATCHDOG+2))
-		FINAL+=("• ")
-		;;
-		"audio-headset") FINAL+=("%{F$COLOR}󱡏%{F-}" ${OUTPUTARRAY[LOOPWATCHDOG+1]}%)
-		((LOOPWATCHDOG=LOOPWATCHDOG+2))
-		FINAL+=("• ")
-		;;
-	esac
-done
-
-unset FINAL[-1]
+declare -A ICONS=( ["input-keyboard"]="%{F$COLOR}󰌌%{F-}" ["audio-headphones"]="%{F$COLOR}󰋋%{F-}" ["audio-card"]="%{F$COLOR}󰓃%{F-}" [audio-headset]="%{F$COLOR}󱡏%{F-}")
 
 if [ "$DEVICE" == "" ]
 	then echo "%{F$COLOR}󰂲%{F-}"
-	else 
-		echo ${FINAL[@]} | awk '{$1=$1};1' | sed 's/󰓃%{F-} %/󰓃%{F-}/g'
-fi
+else
+	for COUNT in $COUNT_CONNECTED; do
+		OUTPUTARRAY+=($(bluetoothctl devices Connected | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep "Icon\|Battery Percentage\|Connected" | sed -n -e 's/^.*Icon: //p' | sed -n ${COUNT}p) $(bluetoothctl devices Connected | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep "Icon\|Battery Percentage\|Connected" | sed -n -e 's/^.*Battery Percentage: //p' | sed -n ${COUNT}p | sed 's/(\(.*\))/\1/' | sed 's/[^ ]* //'))
+	done
 
+	for ELEMENT in ${OUTPUTARRAY[@]}; do
+        if ! [[ $ELEMENT =~ $REXP ]] ; then
+            FINAL+=("${ICONS[$ELEMENT]}" ${OUTPUTARRAY[LOOPWATCHDOG+1]}% "• ")
+            ((LOOPWATCHDOG=LOOPWATCHDOG+2))
+        fi
+	done
+
+	unset FINAL[-1]
+	echo ${FINAL[@]} | awk '{$1=$1};1' | sed 's/󰓃%{F-} %/󰓃%{F-}/g'
+fi
 #%{F$COLOR} %{F-}
