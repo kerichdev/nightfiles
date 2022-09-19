24a52aa606c0171c9dd52e596b841bbc3abededb
kerichdev <kerichuudev@gmail.com>
2022-09-19 21:18:16 +0300

    revamp kde connect module prevent execution of battery encoders if no devices found add device counter

--- .config/polybar/kdec.sh
+++ .config/polybar/kdec.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ~/.config/polybar/colors.sh
+DEVICECOUNT=`kdeconnect-cli -a --id-only 2>&1 | wc -l`
 DEVICEID=`kdeconnect-cli -l --id-only 2>&1 | sed '2,$d'`
 DEVICENAME=`kdeconnect-cli -l --name-only 2>&1 | sed '2,$d'`
 CHARGE=`gdbus call --session --dest org.kde.kdeconnect --object-path /modules/kdeconnect/devices/$DEVICEID/battery --method org.freedesktop.DBus.Properties.Get org.kde.kdeconnect.device.battery charge 2>/dev/null| tr -d '(<' | tr -d '>,)' `
@@ -9,21 +10,25 @@ ERRORCHK=`gdbus call --session --dest org.kde.kdeconnect --object-path /modules/
 BATTERY_ICONS=("󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 BATTERY_ICONS_CHARGING=("󰢟" "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅")
 
-C_INDEX=$((CHARGE * 11 / 100))
-
-if [ "$C_INDEX" -ge 11 ]; then
-        C_INDEX=10
-fi
-
-C_ICON=%{F$COLOR}${BATTERY_ICONS[$C_INDEX]}%{F-}
-CH_ICON=%{F$COLOR}${BATTERY_ICONS_CHARGING[$C_INDEX]}%{F-}
-
 if [ "$ERRORCHK" == "Error" ]
 	then echo "%{F$COLOR}󰄢%{F-}"
-	else 
-		if [ "$CHARGESTATUS" == "true" ]
+	else
+		if [ "$DEVICECOUNT" -gt 1 ]; then
+			echo "%{F$COLOR}󰄡%{F-} $DEVICECOUNT devices"
+		else
+			C_INDEX=$((CHARGE * 11 / 100))
+
+			if [ "$C_INDEX" -ge 11 ]; then
+			        C_INDEX=10
+			fi
+
+			C_ICON=%{F$COLOR}${BATTERY_ICONS[$C_INDEX]}%{F-}
+			CH_ICON=%{F$COLOR}${BATTERY_ICONS_CHARGING[$C_INDEX]}%{F-}
+
+			if [ "$CHARGESTATUS" == "true" ]
 			then echo "%{F$COLOR}󰄡%{F-} $DEVICENAME ($CH_ICON$CHARGE%)"
-		else echo "%{F$COLOR}󰄡%{F-} $DEVICENAME ($C_ICON$CHARGE%)"
+			else echo "%{F$COLOR}󰄡%{F-} $DEVICENAME ($C_ICON$CHARGE%)"
+		fi
 	fi
 fi
 
